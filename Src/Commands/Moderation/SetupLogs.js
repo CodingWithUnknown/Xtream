4e5401ac53c8a43d3d1c48dca59278920dd65064
Unknown Coderz <koushikdasaikyo1414@gmail.com>
2023-09-07 22:44:46 +0600

    Updated

--- Src/Commands/Moderation/SetupLogs.js
+++ Src/Commands/Moderation/SetupLogs.js
@@ -39,6 +39,7 @@ module.exports = {
                         let channel = interaction.options.getChannel('channel');
                         let memberRole = interaction.options.getRole('memberRole');
                         let botRole = interaction.options.getRole('botRole');
+                        
                 }
         }
     }
