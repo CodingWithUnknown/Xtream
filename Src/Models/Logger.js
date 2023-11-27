ecaef2f2e9bf4466312b48bd165b06f710570e59
CodingWithUnknown <koushikdasaikyo1414@gmail.com>
2023-11-28 02:07:55 +0600

    Fixed

--- Src/Models/Logger.js
+++ Src/Models/Logger.js
@@ -6,28 +6,28 @@ module.exports = class Logger {
     let date = `${moment().format('DD-MM-YYYY hh:mm:ss')}`;
     switch (type) {
       case 'log': {
-        return console.log(`[${colors.gray(date)}]: [${colors.blue.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+        return console.log(`[${colors.gray(date)}]: [${colors.blue(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
       }
       case 'warn': {
-        return console.log(`[${colors.gray(date)}]: [${colors.yellow.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+        return console.log(`[${colors.gray(date)}]: [${colors.yellow(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
       }
       case 'debug': {
-        return console.log(`[${colors.gray(date)}]: [${colors.blue.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+        return console.log(`[${colors.gray(date)}]: [${colors.blue(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
       }
       case 'ready': {
-        return console.log(`[${colors.gray(date)}]: [${colors.green.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
-      }
-      case 'system': {
-        return console.log(`[${colors.gray(date)}]: [${colors.magenta.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+        return console.log(`[${colors.gray(date)}]: [${colors.green(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
       }
       case 'event': {
-        return console.log(`[${colors.gray(date)}]: [${colors.yellow.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+        return console.log(`[${colors.gray(date)}]: [${colors.yellow(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+      }
+      case 'system': {
+        return console.log(`[${colors.gray(date)}]: [${colors.magenta(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
       }
       case 'error': {
-        return console.log(`[${colors.gray(date)}]: [${colors.red.bgBlack(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.blue(content)}`);
+        return console.log(`[${colors.gray(date)}]: [${colors.red(type.charAt(0).toUpperCase() + type.slice(1))}] ${colors.gray(content)}`);
       }
       default:
-        throw new TypeError('Logger type must be either log, warn, debug, ready, slash, event or error.');
+        throw new TypeError('Logger type must be either log, warn, debug, ready, event, system or error.');
     }
   }
 };
