edce7b0f730c488d85b7196145d47371fd23dd07
CodingWithUnknown <koushikdasaikyo1414@gmail.com>
2023-12-14 23:19:38 +0600

    Updated

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 require('dotenv').config({ path: '.env' });
-const { ShardingManager } = require('discord.js');
+const { ShardingManager, ShardEvents } = require('discord.js');
 
-let manager = new ShardingManager('./Src/Xtream.js', { token: process.env.DISCORD_TOKEN, respawn: true });
+let manager = new ShardingManager('./Src/Xtream.js', { token: process.env.DISCORD_TOKEN, respawn: true, totalShards: 'auto', shardList: 'auto', mode: 'process' });
 
 manager.once('shardCreate', async (shard) => {
   shard.logger = require('./Src/Models/Logger');
-  await shard.logger.log(`Shard system has been deployed: ${shard.id + 1}/${manager.totalShards}`, 'system')
+  await shard.logger.log(`Shard system ${shard.id + 1}/${manager.totalShards} connected to Discord's Gateway`, 'system');
 
-  shard.once('shardDisconnect', async () => {
-    await shard.logger.log(`Shard system ${shard.id + 1}/${manager.totalShards} disconnected.`, 'system')
+  shard.once(ShardEvents.Ready, async () => {
+    await shard.logger.log(`Shard system ${shard.id + 1}/${manager.totalShards} Ready.`, 'system');
   });
 
-  shard.once('shardReconnecting', async () => {
-    await shard.logger.log(`Reconnecting shard system ${shard.id + 1}/${manager.totalShards}`, 'system')
+  shard.once(ShardEvents.Disconnect, async () => {
+    await shard.logger.log(`Shard system ${shard.id + 1}/${manager.totalShards} disconnected.`, 'system');
   });
 
-  shard.once('death', async (process) => {
-    await shard.logger.log(`Closing shard system ${shard.id + 1}/${manager.totalShards} unexpectedly.`, 'system')
+  shard.once(ShardEvents.Reconnecting, async () => {
+    await shard.logger.log(`Reconnecting shard system ${shard.id + 1}/${manager.totalShards}`, 'system');
+  });
+
+  shard.once(ShardEvents.Death, async (process) => {
+    await shard.logger.log(`Closing shard system ${shard.id + 1}/${manager.totalShards} unexpectedly.`, 'system');
 
     if (process.exitCode === null) {
-      await shard.logger.log(`Shard system ${shard.id + 1}/${manager.totalShards} exited.\n\n**PID** - \`${process.pid}\`\n**Code** - \`${process.exitCode}\``, 'system')
+      await shard.logger.log(`Shard system ${shard.id + 1}/${manager.totalShards} exited.\n\n**PID** - \`${process.pid}\`\n**Code** - \`${process.exitCode}\``, 'system');
     }
   });
 });
 
-manager.spawn();
+manager.spawn({ amount: 'auto', delay: null, timeout: -1 });
