83f3d9819952800f989419e5d2a5553af9ab7e4c
CodingWithUnknown <73554280+CodingWithUnknown@users.noreply.github.com>
2024-01-06 01:43:17 +0600

    files updated successfully

--- Src/Events/Client/InteractionCreate.js
+++ Src/Events/Client/InteractionCreate.js
@@ -1,5 +1,5 @@
 const { Client, Events, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
-const Ostracize = require('../../Models/Schema/Ostracize');
+const Suspention = require('../../Models/Schema/Suspention');
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -14,19 +14,15 @@ module.exports = {
       let commands = await client.commands.get(interaction.commandName);
       if (!commands) {
         const embeds = new EmbedBuilder()
-          .setAuthor({ name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
           .setDescription('This (/) command is outdated.')
-          .setFooter({ text: 'Xtream developers', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
-          .setColor(0x2d2c31)
-          .setTimestamp();
+          .setColor(0x2d2c31);
         return await interaction.reply({ embeds: [embeds], ephemeral: true });
       }
 
-      await Ostracize.findOne({ User: interaction.user.id }, async (err, data) => {
+      await Suspention.findOne({ User: interaction.user.id }, async (err, data) => {
         if (err) throw err;
         if (data) {
           const embeds = new EmbedBuilder()
-            .setAuthor({ name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
             .setDescription('You have been Suspended from Xtream Developers.')
             .setColor(0x2d2c31);
           return await interaction.reply({ embeds: [embeds], ephemeral: true });
@@ -34,48 +30,12 @@ module.exports = {
       });
 
       if (!interaction.channel.permissionsFor(interaction.guild.members.me).has(PermissionFlagsBits.ViewChannel && PermissionFlagsBits.SendMessages)) return;
-
       if (!interaction.channel.permissionsFor(interaction.guild.members.me).has(PermissionFlagsBits.EmbedLinks)) return await interaction.reply({ content: `I don't have \`EmbedLinks\` permission to use this command.`, ephemeral: true });
-
-      // if (!interaction.channel.permissionsFor(interaction.guild.members.me).has(PermissionFlagsBits.Administrator)) return await interaction.reply({ content: 'I don\'t have enough permissions to use this command.' });
-
-      /*if (Command.clientPerms && !interaction.channel.permissionsFor(interaction.guild.members.me).has(PermissionsBitField.resolve(Command.clientPerms))) {
-        const embeds = new EmbedBuilder()
-          .setAuthor(
-            { name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ dynamic: true }) }
-          )
-          .setDescription(`I need to this \`${Command.clientPerms.join(', ')}\` permissions to use this command.`)
-          .setFooter(
-            { text: 'Xtream Developers', iconURL: client.user.displayAvatarURL({ dynamic: true }) }
-          )
-          .setColor(0x2d2c31)
-          .setTimestamp();
-        return await interaction.reply({ embeds: [embeds], ephemeral: true });
-      }
-  
-      if (Command.userPerms && !interaction.channel.permissionsFor(interaction.member).has(PermissionsBitField.resolve(Command.userPerms))
-      ) {
-        const embeds = new EmbedBuilder()
-          .setAuthor(
-            { name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ dynamic: true }) }
-          )
-          .setDescription(`You need to this \`${Command.userPerms.join(', ')}\` permissions to use this command.`
-          )
-          .setFooter(
-            { text: 'Xtream Developers', iconURL: client.user.displayAvatarURL({ dynamic: true }) }
-          )
-          .setColor(0x2d2c31)
-          .setTimestamp();
-        return await interaction.reply({ embeds: [embeds], ephemeral: true });
-      }*/
 
       if (commands.developer && !client.developer.includes(interaction.user.id)) {
         const embeds = new EmbedBuilder()
-          .setAuthor({ name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
           .setDescription('This command can only be Managed or Executed by the [Developer](https://discord.com/users/770887288464867338).')
-          .setFooter({ text: 'Xtream Developers', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
-          .setColor(0x2d2c31)
-          .setTimestamp();
+          .setColor(0x2d2c31);
         return await interaction.reply({ embeds: [embeds], ephemeral: true });
       }
 
@@ -88,11 +48,8 @@ module.exports = {
             return await interaction.followUp({ content: 'There was an error executing that command.', ephemeral: true });
           } else {
             const embeds = new EmbedBuilder()
-              .setAuthor({ name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
               .setDescription('There was an error executing that command.')
-              .setFooter({ text: 'Xtream Developers', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
-              .setColor(0x2d2c31)
-              .setTimestamp();
+              .setColor(0x2d2c31);
             return await interaction.followUp({ embeds: [embeds], ephemeral: true });
           }
         } else {
@@ -100,11 +57,8 @@ module.exports = {
             return await interaction.reply({ content: 'There was an error executing that command.', ephemeral: true });
           } else {
             const embeds = new EmbedBuilder()
-              .setAuthor({ name: 'Xtream Defender', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
               .setDescription('There was an error executing that command.')
-              .setFooter({ text: 'Xtream Developers', iconURL: client.user.displayAvatarURL({ forceStatic: true }) })
-              .setColor(0x2d2c31)
-              .setTimestamp();
+              .setColor(0x2d2c31);
             return await interaction.reply({ embeds: [embeds], ephemeral: true });
           }
         }
--- Src/Models/Schema/Suspention.js
+++ Src/Models/Schema/Suspention.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-module.exports = model('Ostracize', new Schema({
+module.exports = model('Suspention', new Schema({
   _id: Schema.Types.ObjectId,
   User: { type: String },
 }));
